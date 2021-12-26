45f3a4d1dbe2abecb712fb8c097737ee021dd946
bbosmedia <bbosmedia@users.noreply.github.com>
2021-12-26 23:49:13 +0500

    Sub Restaurant Page is Added

--- src/pages/SubRestaurantPage/SubReatauranPage.jsx
+++ src/pages/SubRestaurantPage/SubReatauranPage.jsx
@@ -26,16 +26,21 @@ const SubReatauranPage = ({lang}) => {
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb ya-breadcrumb">
         <li className="breadcrumb-item">
-
-          <Link to="/" className="nuxt-link-active"> Главная </Link>
+            {lang === '' && <Link to="/" className="nuxt-link-active"> Главная </Link>}
+            {lang === '/uz' && <Link to="/uz" className="nuxt-link-active"> Bosh Sahifa </Link>}
+            {lang === '/en' && <Link to="/en" className="nuxt-link-active"> Home</Link>}
         </li>
         <li className="breadcrumb-item">
-          <Link to="/restaurants" className="nuxt-link-active"> Рестораны </Link>
+            {lang === '' && <Link to="/restaurants" className="nuxt-link-active"> Рестораны </Link>}
+            {lang === '/uz' && <Link to="/uz/restaurants" className="nuxt-link-active"> Restoranlar</Link>}
+            {lang === '/ru' && <Link to="/ru/restaurants" className="nuxt-link-active"> Restaurants </Link>}
         </li>
         <li aria-current="page" className="breadcrumb-item active"> Yaponamama Шота </li>
       </ol>
     </nav>
-    <h1 className="ya-group-title">О ресторане</h1>
+    {lang === '' && <h1 className="ya-group-title">О ресторане</h1>}
+    {lang === '/uz' && <h1 className="ya-group-title">Restoran haqida</h1>}
+    {lang === '/en' && <h1 className="ya-group-title">About restaurant</h1>}
     <div className="row">
       <div className="col-md-8">
         <div className="ya-restaurant-text">
@@ -45,15 +50,11 @@ const SubReatauranPage = ({lang}) => {
           {lang === '' &&  <p><strong>Количество мест в зале</strong>: на {items.seatCount} персоны</p>}
           {lang === '/uz' &&  <p><strong>Zaldagi o'rinlar soni</strong>: {items.seatCount} kishilik</p>}
           {lang === '/en' &&  <p><strong>Number of seats</strong>: for {items.seatCount} people</p>}
-          <p>
-            <strong>Парковка:</strong> есть
-          </p>
+          <p><strong>Парковка:</strong> есть</p>
           {lang === '' && <p><strong>Режим работы:</strong> {items.workingTime}&nbsp;</p>}
           {lang === '/uz' && <p><strong>Ish vaqti:</strong> {items.workingTime}&nbsp;</p>}
           {lang === '/en' && <p><strong>Working hours:</strong> {items.workingTime}&nbsp;</p>}
-          <p>
-            <strong>Резерв столов:</strong>&nbsp; <a href="tel:+998982511213">+99898-251-12-13</a>; <a href="tel:+998712531213">+99871-253-12-13</a>
-          </p>
+          <p><strong>Резерв столов:</strong>&nbsp; <a href="tel:+998982511213">+99898-251-12-13</a>; <a href="tel:+998712531213">+99871-253-12-13</a></p>
           {lang === '' && <><p><strong>Адрес:</strong> {items.addressRu}</p><p><strong>Ориентир:</strong> {items.preferencePointRu}</p></>}
           {lang === '/uz' && <><p><strong>Manzil:</strong> {items.addressUz}</p><p><strong>Mo'ljal:</strong> {items.preferencePointUz}</p></>}
           {lang === '/en' && <><p><strong>Address:</strong> {items.addressEn}</p><p><strong>Landmark:</strong> {items.preferencePointEn}</p></>}
